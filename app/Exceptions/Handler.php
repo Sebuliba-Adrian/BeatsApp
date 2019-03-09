5e29dc711039779b8f009a2359380435d864fd4f
adrian <adrian.sebuliba1@gmail.com>
2019-03-09 16:47:25 +0300

    ft(authentication): setup laravel passport for authentication

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        return response()->json(
+            [
+                'errors' => [
+                    'status' => 401,
+                    'message' => 'Unauthenticated',
+                ]
+            ],
+            401
+        );
     }
 }
