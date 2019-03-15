ce890ec88e4ef7937497c096d2b3ab360d6c2062
adrian <adrian.sebuliba1@gmail.com>
2019-03-15 05:08:47 +0300

    feat(transformers): Add resources

---
+++ app/Http/Resources/PlaylistResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PlaylistResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'tracks' => $this->tracks,
+            'creator' => $this->user,
+        ];
+    }
+}
