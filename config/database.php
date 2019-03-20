32a8c9626ca6d6eb76e6535d287663ff6378cbd0
adrian <adrian.sebuliba1@gmail.com>
2019-03-20 22:47:48 +0300

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+$DATABASE_URL = parse_url( 'postgres://mpkityovhonwmy:2957536a931db77e40f92f60f5d688e9b5b91a7c993f1b6040a68ea10063ea3a@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d2pf1m2tspasns');
 
 return [
 
@@ -61,11 +62,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
